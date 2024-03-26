9e5d77c1a068cca84af045ec7aa34906a57ca306
Pedro Gonçalves Moreira <pedrogonmoreira@gmail.com>
2024-03-25 22:30:16 -0300

    criação do card

--- pokedex/src/Pages/Home/index.js
+++ pokedex/src/Pages/Home/index.js
@@ -1,22 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
+import '../../styles/Home.css';
+import '../../styles/Types.css';
 
 export default function Home() {
-  const [pokemon, setPokemon] = useState([]);
+  const [pokemonList, setPokemonList] = useState([]);
 
   useEffect(() => {
-    async function getPokemon() {
-      const response = await api.get('pokemon?limit=2000&offset=0');
-      setPokemon(response.data.results);
-      console.log(pokemon)
+    async function fetchPokemon() {
+      try {
+        const response = await api.get('pokemon?limit=2000&offset=0');
+        const results = response.data.results;
+        const pokemons = await Promise.all(
+          results.map(async (pokemon) => {
+            const pokemonResponse = await api.get(pokemon.url);
+            return pokemonResponse.data;
+          })
+        );
+        console.log(pokemons)
+        setPokemonList(pokemons);
+      } catch (error) {
+        console.error('Error fetching Pokémon:', error);
+      }
     }
-    getPokemon();
-  }, []);
+    fetchPokemon();
+  }, []); 
 
   return (
-    <div>
-      {pokemon.map((pokemon, index) => (
-        <div key={index}>
+    <div style={{ display: 'flex', justifyContent: 'space-around', flexDirection: 'row', flexWrap: 'wrap' }}>
+      {pokemonList.map((pokemon, index) => (
+        <div key={index} className={`card-pkm ${pokemon.types[0].type.name}`}>
+          <img src={pokemon.sprites.front_default} alt="Pokemon Sprite" />
           <p>{pokemon.name}</p>
         </div>
       ))}
