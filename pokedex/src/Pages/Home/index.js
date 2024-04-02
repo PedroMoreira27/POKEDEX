796d645835d68389c1dfc7d9d172246253b8e21c
PEDRO GONCALVES MOREIRA <1091392313013@fatec-srv.local>
2024-04-02 20:32:43 -0300

    aula

--- pokedex/src/Pages/Home/index.js
+++ pokedex/src/Pages/Home/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
+import Navbar from '../../components/navbar';
 import '../../styles/Home.css';
 import '../../styles/Types.css';
 
@@ -15,25 +16,35 @@ export default function Home() {
           results.map(async (pokemon) => {
             const pokemonResponse = await api.get(pokemon.url);
             return pokemonResponse.data;
-          })
+          }),
         );
-        console.log(pokemons)
+        console.log(pokemons);
         setPokemonList(pokemons);
       } catch (error) {
         console.error('Error fetching Pokémon:', error);
       }
     }
     fetchPokemon();
-  }, []); 
+  }, []);
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-around', flexDirection: 'row', flexWrap: 'wrap' }}>
-      {pokemonList.map((pokemon, index) => (
-        <div key={index} className={`card-pkm ${pokemon.types[0].type.name}`}>
-          <img src={pokemon.sprites.front_default} alt="Pokemon Sprite" />
-          <p>{pokemon.name}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <Navbar />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+        }}
+      >
+        {pokemonList.map((pokemon, index) => (
+          <div key={index} className={`card-pkm ${pokemon.types[0].type.name}`}>
+            <img src={pokemon.sprites.front_default} alt="Pokemon Sprite" />
+            <p>{pokemon.name}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
