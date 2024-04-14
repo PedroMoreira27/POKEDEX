436fdb654070e4ed559eb84f7fb3866ef5877402
Pedro Gonçalves Moreira <pedrogonmoreira@gmail.com>
2024-04-14 19:59:25 -0300

    vite

--- pokedex/vite.config.js
+++ pokedex/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
-import React from 'react';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [React()],
-  base: '/POKEDEX/pokedex',
+  plugins: [react()],
+  base: '/POKEDEX/pokedex/',
 });
